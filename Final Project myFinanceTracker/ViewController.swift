787957b34d4c98eb2f61be71d0b1707b24d36375
victor <haibei.wu@rbc.com>
2016-07-11 01:56:20 -0400

    try charts library

--- Final Project myFinanceTracker/ViewController.swift
+++ Final Project myFinanceTracker/ViewController.swift
@@ -7,12 +7,63 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var barChartView: HorizontalBarChartView!
+    @IBOutlet weak var pieChartView: PieChartView!
+    
+    var months:Array<String>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        months = ["Mortgage", "Taxes", "Education", "Utilities", "Insurance", "Groceries", "Restaruant"]
+        let unitsSold = [200.0, 400.0, 600.0, 300.0, 100.0, 50.0, 101.1]
+        
+        setChart(months, values: unitsSold)
+        
+        barChartView.noDataText = "You need to provide data for the chart."
+    }
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Units Sold")
+        let chartData = BarChartData(xVals: months, dataSet: chartDataSet)
+        barChartView.data = chartData
+        barChartView.xAxis.labelPosition = .Bottom
+        barChartView.getAxis(ChartYAxis.AxisDependency.Right).enabled = false
+        barChartView.getAxis(ChartYAxis.AxisDependency.Left).enabled = false
+        barChartView.descriptionText = ""
+        barChartView.legend.enabled = false
+        barChartView.getAxis(ChartYAxis.AxisDependency.Left).axisMaxValue = 600
+        barChartView.getAxis(ChartYAxis.AxisDependency.Left).axisMinValue = 0
+        barChartView.drawValueAboveBarEnabled = false
+        
+        let pieChartDataSet = PieChartDataSet(yVals: dataEntries, label: "Units Sold")
+        let pieChartData = PieChartData(xVals: dataPoints, dataSet: pieChartDataSet)
+        pieChartView.data = pieChartData
+        
+        var colors: [UIColor] = []
+        
+        for _ in 0..<dataPoints.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+        }
+        
+        pieChartDataSet.colors = colors
     }
 
     override func didReceiveMemoryWarning() {
